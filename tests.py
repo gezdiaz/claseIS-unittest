a2075ec20557db10dfd4f93e8f936d8224aed94e
Gaston <gastonezequieldiaz@outlook.com>
2019-11-08 20:26:28 -0300

    pruebas corregidas

--- tests.py
+++ tests.py
@@ -4,7 +4,10 @@ from datetime import datetime, timedelta
 
 class TestLog(unittest.TestCase):
     def test_1(self):
-        hoy = datetime(2019, 11, 8)
+        hoy = datetime.now()
         fechaNacimiento = datetime(2000, 11, 8)
         edad = (hoy - fechaNacimiento) // timedelta(days=365.2425)
         self.assertEqual(edad, Edad().edad(fechaNacimiento))
+
+if __name__ == '__main__':
+    unittest.main()
